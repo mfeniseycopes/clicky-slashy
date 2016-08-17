50241b8cd9e3ef1edd6dc56027981ba14472844a
Meagan <mfeniseycopes@gmail.com>
2016-08-17 14:07:21 -0400

    setup npm

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  context: __dirname,
+  entry: "./clicky_slashy.jsx",
+  output: {
+    path: "./",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
